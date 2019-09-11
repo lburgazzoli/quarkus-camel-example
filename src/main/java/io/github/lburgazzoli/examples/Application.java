650821da4e9f6f276bea1ff3a691df2f0d25a738
lburgazzoli <lburgazzoli@gmail.com>
2019-09-11 10:37:01 +0200

    Initial import

---
+++ src/main/java/io/github/lburgazzoli/examples/Application.java
@@ -0,0 +1,22 @@
+package io.github.lburgazzoli.examples;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.apache.camel.quarkus.core.runtime.CamelRuntime;
+import org.apache.camel.quarkus.core.runtime.StartedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    @Inject
+    CamelRuntime runtime;
+    
+    void started(@Observes StartedEvent event) {
+        LOGGER.info("my.property = {}", runtime.getContext().resolvePropertyPlaceholders("{{my.property}}"));
+    }
+}
